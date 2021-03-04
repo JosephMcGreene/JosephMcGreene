fa27b207946d875accd4fd913a91ef1590233e7e
Joseph McGreene <josephgreene693@gmail.com>
2021-03-04 14:43:24 -0800

    First Commit, sort of

---
+++ js/audio.js
@@ -0,0 +1,27 @@
+const pianoConcerto1 = new MediaElementPlayer(document.querySelector('#piano-concerto-1'), {
+  pluginPath: "/path/to/shims/",
+  success: function(mediaelement, originalNode, instance) {
+
+  }
+});
+
+const pianoConcerto4 = new MediaElementPlayer(document.querySelector('#piano-concerto-4'), {
+  pluginPath: "/path/to/shims/",
+  success: function(mediaelement, originalNode, instance) {
+
+  }
+});
+
+const stringQuintet1 = new MediaElementPlayer(document.querySelector('#string-quintet-1'), {
+  pluginPath: "/path/to/shims/",
+  success: function(mediaelement, originalNode, instance) {
+
+  }
+});
+
+const stringQunitet2 = new MediaElementPlayer(document.querySelector('#string-quintet-2'), {
+  pluginPath: "/path/to/shims/",
+  success: function(mediaelement, originalNode, instance) {
+
+  }
+});
