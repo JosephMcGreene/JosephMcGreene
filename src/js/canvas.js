9b7a742b69acfcd5de3d07ffe8ddf958af813e33
JosephMcGreene <josephgreene693@gmail.com>
2021-04-06 15:34:53 -0700

    Add homepage content, move html files and associated file paths

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,37 +1,35 @@
+const button = document.querySelector('#button');
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth - 16;
+canvas.width = window.innerWidth - 16.5;
 canvas.height = 600;
 
-const hexAngle = 2 * Math.PI / 6;
-let radius = 0;
+// let hexagon = new Hexagon(45);
 
-// function drawHexagon(x, y, hexRad) {
-//   radius = hexRad;
-//   ctx.beginPath();
-//   const hexCenter = ctx.moveTo(x, y);
+function generateHex() {
+  const rad = Math.floor(Math.random() * Math.floor(50) + 1);
 
-//   ctx.moveTo(x, y - hexRad);
-//   let point1 = ctx.moveTo(x, y - hexRad);
-//   ctx.lineTo( Math.sqrt( Math.pow(hexRad, 2) - Math.pow((hexRad / 2), 2) ), point1 + (hexRad / 2) );
+  let hexagon = new Hexagon(rad); // see src/js/Hexagon.js
 
-//   ctx.stroke();
-//   // ctx.fillStyle = 'black';
-//   // ctx.fill();
-// }
-function drawHexagon(x, y, hexRad) {
-  radius = hexRad;
-  ctx.beginPath();
-  ctx.rotate(30 * Math.PI / 180);
-
-  for (var i = 0; i < 6; i++) {
-    ctx.lineTo(x + radius * Math.cos(hexAngle * i), y + radius * Math.sin(hexAngle * i));
-  }
-  ctx.closePath();
-  ctz.fillStyle = 'black';
-  ctx.fill();
+  return hexagon;
 }
 
-drawHexagon(200,200, 50);
+function animateHex() {
+  ctx.clearRect(generateHex().xPos - generateHex().radius - 4,
+                generateHex().yStart - generateHex().radius,
+                (generateHex().radius * 2) + 2,
+                generateHex().radius * 2.1);
 
+  generateHex().draw();
+
+  generateHex().xPos += generateHex().dirX;
+  generateHex().yStart += generateHex().dirY;
+
+  requestAnimationFrame(animateHex);
+}
+
+button.addEventListener("click", () => {
+  generateHex().draw();
+  animateHex();
+});
