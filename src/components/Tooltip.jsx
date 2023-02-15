2dbb1d7295d74bbbd4f14433f283f74e7f7e9c75
Joseph McGreene <josephorianmcgreene@gmail.com>
2023-02-15 12:30:03 +0100

    Add personal logo

--- src/components/Tooltip.jsx
+++
@@ -1,8 +0,0 @@
-export default function Tooltip({ text }) {
-  return (
-    <aside className="tooltip">
-      <div className="tooltip-arrow"></div>
-      <p className="tooltip-content">{text}</p>
-    </aside>
-  );
-}
